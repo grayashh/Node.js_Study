0601ac1c07bfcb2b5b320af87fb9f54ebdf20c0b
kimtaekang <k_2684@naver.com>
2022-09-21 01:41:10 +0900

    내용 추가

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const http = require("http");
+import http from "http";
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-
+  const url = req.url;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My first page</title></head>");
