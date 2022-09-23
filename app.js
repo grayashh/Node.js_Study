5d7b11454966b0877f574069c5246f1584057f8f
kimtaekang <k_2684@naver.com>
2022-09-24 01:37:15 +0900

    미들웨어 라우팅

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import express from "express";
+import bodyParser from "body-parser";
+import adminRoutes from "./routes/admin.js";
+import shopRoutes from "./routes/shop.js";
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next(); // Allows the request to continue to the next middleware in line
-});
+app.use(bodyParser.urlencoded());
+
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  console.log("In another middleware!");
-  res.send("<h1>Hello from Express!</h1>");
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3000);
