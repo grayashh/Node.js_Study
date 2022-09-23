24d397f493b56b4a70dc8f16e5d783df9e81f705
kimtaekang <k_2684@naver.com>
2022-09-23 13:38:00 +0900

    요청 본문 해석

--- app.js
+++ app.js
@@ -3,20 +3,31 @@ import fs from "fs";
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
     res.write(
-      '<body><form action="/message" method="POST"><input type="text name="message"><button type="submit">Send</button></form></body>'
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write("</html>");
     return res.end();
   }
-  if (url === "/message" && req.method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY");
-    res.writeHead(302, { Location: "/" });
-    res.end();
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
